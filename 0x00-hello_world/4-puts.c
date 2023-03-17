140bd82fc4337b8f707f96463429ad23a3f9df5d
mohamednayef <mohamednayef2002@gmail.com>
2023-03-16 18:10:33 -0700

    lkj

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio>
+/**
+*that is comment for me
+*/
+int main(void)
+{
+	/**
+	*that is short discription
+	*/
+	/**
+	*this descrtion
+	*/
+	puts("\"Programming is like building a multilingual puzzle");
+	/**
+	*this for return
+	*/
+	return (0);
+}
